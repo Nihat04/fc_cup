c45002d8fdb9aaf5f855aa00eb773be8d24ed6a4
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2024-07-02 22:27:47 +0500

    Add user context, forum, comment...

---
+++ back/FcCupApi/FcCupApi/Contexts/UsersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FcCupApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FcCupApi.Contexts
+{
+    public class UsersDbContext : IdentityDbContext<User>
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Forum> Forums { get; set; }
+    }
+}
---
+++ back/FcCupApi/FcCupApi/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FcCupApi.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime RegistrationDateTime { get; set; }
+        public List<Comment>? Comments { get ; set; }
+        public Club? FavouriteClub { get; set; }
+        public Player? FavouritePlayer { get; set; }
+        public List<Club>? FollowedClubs {  get; set; }
+        public List<Player>? FollowedPlayers { get; set; }
+        public Status? CommunityStatus { get; set; }
+        // rating 
+    }
+
+    public enum Status
+    {
+        Loh,
+        OldMan,
+        NewFag,
+        RonaldoSon
+    }
+}
