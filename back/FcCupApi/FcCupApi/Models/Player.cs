bf52ae6f08ce9cc7869d78ee8663b89621c45345
Nihat04 <aliev.nihat4@gmail.com>
2024-07-14 14:36:47 +0500

    header is done

--- back/FcCupApi/FcCupApi/Models/Player.cs
+++ back/FcCupApi/FcCupApi/Models/Player.cs
@@ -3,7 +3,8 @@
     public class Player
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string? Name { get; set; }
         public string ImageUrl { get; set; }
         public List<TournamentPlayer> Tournaments { get; set; }
         public List<Statistic<Player>> Stats { get; set; }
